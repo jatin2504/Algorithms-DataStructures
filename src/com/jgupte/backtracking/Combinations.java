589bb1d06836d33e4a6bb63455faad77f38b39a2
Jatin Gupte <jatin.gupte25@gmail.com>
2020-01-08 22:05:17 -0500

    improved code for combinations program

--- src/com/jgupte/backtracking/Combinations.java
+++ src/com/jgupte/backtracking/Combinations.java
@@ -3,46 +3,22 @@ package com.jgupte.backtracking;
 import java.util.*;
 
 public class Combinations {
-    List<List<Integer>> combinations = new ArrayList<>();
-
-    public void getCombinations(int[] A) {
-        Map<Integer, Integer> countMap = new HashMap<Integer, Integer>();
-        for (int a : A) {
-            countMap.compute(a, (key, val) -> {
-                if (val == null)
-                    return 1;
-                else
-                    return val + 1;
-            });
-        }
-
-        int[] count = new int[countMap.size()];
-        int[] input = new int[countMap.size()];
-        int i = 0;
-        for (Map.Entry<Integer, Integer> entry : countMap.entrySet()) {
-            count[i] = entry.getValue();
-            input[i] = entry.getKey();
-            i++;
-        }
-        int[] output = new int[A.length];
-
-
-        backtrack(input, count, 0, output, 0);
-
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        List<Integer> subset = new ArrayList<>();
+        subsetsUtil(nums, subset, result, 0);
+        return result;
     }
 
-    private void backtrack(int[] input, int[] count, int pos, int[] output, int len) {
-        print(output, len);
-        for (int i = pos; i < input.length; i++) {
-            if (count[i] == 0)
-                continue;
-
-            output[len] = input[i];
-            count[i]--;
-            backtrack(input, count, i, output, len + 1);
-            count[i]++;
+    private void subsetsUtil(int[] nums, List<Integer> subset, List<List<Integer>> result, int index) {
+        result.add(new ArrayList<>(subset));
+        for (int i = index; i < nums.length; i++){
+            subset.add(nums[i]);
+            subsetsUtil(nums, subset, result, i+1);
+            subset.remove(subset.size() - 1);
         }
     }
+
 
     private void print(int[] output, int len) {
         for (int i = 0; i < len; i++)
@@ -52,14 +28,6 @@ public class Combinations {
 
     public static void main(String[] args) {
         Combinations combinations = new Combinations();
-        combinations.getCombinations(new int[]{1, 2, 3});
-        PriorityQueue<Integer> q = new PriorityQueue<>((o1, o2) -> o1 - o2);
-        q.offer(3);
-        q.add(6);
-        q.add(2);
-        q.add(7);
-        q.offer(5);
-
-        System.out.println(q.peek());
+        System.out.println(combinations.subsets(new int[]{1, 2, 3}));
     }
 }
